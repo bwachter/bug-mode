1a1047981eb677ac856a65febee8faf098d3af00
Bernd Wachter <bwachter-spam@lart.info>
2016-04-19 16:17:07 +0300

    Make sure rally-specific fields don't clash with bugzilla extensions

--- lisp/bz-bug-mode.el
+++ lisp/bz-bug-mode.el
@@ -114,7 +114,7 @@
                      ;; TODO: implement retrieval of additional rally objects
                      ;;       until it's implemented just hide them to avoid
                      ;;       polluting the bug buffer
-                     (not (equal 8 (bz--bug-get-field-property (car prop) 'type)))
+                     (not (equal 98 (bz--bug-get-field-property (car prop) 'type)))
                      (not (equal (cdr prop) nil))
                      (not (string= (car prop) "internals")))) bug) "\n"))
 
